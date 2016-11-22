99c15adbb4eb689030184348f55b026b6544680d
diegon <diegon@MIW-NB33>
2016-11-22 14:58:09 -0300

    dudas en código con el Main

---
+++ Math/ConsoleApplication1/TutorialesYa/Declaración Clases/Punto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.TutorialesYa.Declaración_Clases
+{
+    class Punto
+    {
+        private int x, y;
+
+        public void Carga()
+        {
+            Console.WriteLine("Ingrese el valor de x:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de y:");
+            y = int.Parse(Console.ReadLine());
+        }
+
+        public void Imprimir()
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("El punto se ubica en el Primer Cuadrante.");
+                Console.ReadKey();
+            }
+            else
+            {
+                if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("El punto se ubica en el Segundo Cuadrante.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    if (x < 0 && y < 0)
+                    {
+                        Console.WriteLine("El punto se ubica en el Tercer Cuadrante.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        if (x > 0 && y < 0)
+                        {
+                            Console.WriteLine("El punto se ubica en el Cuarto Cuadrante.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El punto no se encuentra en ningún cuadrante.");
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+            }
+
+            static void Main (String[) args){
+
+                Punto punto1 = new Punto();
+                punto1.
+                
+                                        
+        }
+    }
+}
---
+++ Math/ConsoleApplication1/TutorialesYa/Declaración Clases/Triangulo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.TutorialesYa.Declaración_Clases
+{
+    class Triangulo
+    {
+        private int lado1, lado2, lado3;
+
+        public void Inicializar()
+        {
+            Console.WriteLine("Ingrese el Lado 1 del triangulo:");
+            lado1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Lado 2 del triangulo:");
+            lado2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Lado 3 del triangulo:");
+            lado3 = int.Parse(Console.ReadLine());
+        }
+
+        public void LadoMayor()
+        {
+            if (lado1 > lado2 && lado1 > lado3)
+            {
+                Console.WriteLine("El lado 1 es el mayor. Su valor es: " + lado1);
+            }
+            else
+            {
+                if (lado2 > lado3)
+                {
+                    Console.WriteLine("El lado 2 es el mayor. Su valor es: " +lado2);
+                }
+                else
+                {
+                    Console.WriteLine("El lado 3 es el mayor. Su valor es " +lado3);
+                }
+            }
+        }
+
+        public void Equilatero()
+        {
+            if (lado1 == lado2 && lado3 == lado2)
+            {
+                Console.WriteLine("El traingulo es equilatero.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("El triangulo no es equilatero.");
+                Console.ReadKey();
+            }
+        }
+
+        static void Main (String[] args)
+        {
+            Triangulo tri = new Triangulo();
+            tri.Inicializar();
+            tri.LadoMayor();
+            tri.Equilatero();
+            Console.ReadKey();
+        }        
+    }
+}

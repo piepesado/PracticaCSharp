711c0fcb27fb4e27cf359d03fd3a414404997087
diegon <diegon@MIW-NB33>
2016-11-25 12:55:45 -0300

    Se creó programa Empresa.

---
+++ Math/ConsoleApplication1/TutorialesYa/Vectores/Empresa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.TutorialesYa.Vectores
+{
+    class Empresa
+    {
+        private float[] sueldosMañana;
+        private float[] sueldosTarde;
+
+        public void CargaSueldos()
+        {
+            sueldosMañana = new float[4];
+            sueldosTarde = new float[4];
+            Console.WriteLine("Ingrese el sueldo del empelado turno mañana:");
+            for (int f = 0; f < 4; f++)
+            {
+                Console.Write("Ingrese Sueldo:");
+                sueldosMañana[f] = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Ingrese el sueldo del empleado turno noche:");
+            for (int f = 0; f < 4; f++)
+            {
+                Console.Write("Ingrese Sueldo:");
+                sueldosTarde[f] = float.Parse(Console.ReadLine());
+            }
+        }
+        public void ImprimeGastos()
+        {
+            float man = 0;
+            float tar = 0;
+            for (int f = 0; f < 4; f++)
+            {
+                man = man + sueldosMañana[f];
+                tar = tar + sueldosTarde[f];
+            }
+            Console.WriteLine("Los gastos para el turno de la mañana son de " + man + " pesos.");
+            Console.WriteLine("Los gastos para el turno de la tarde son de " + tar + " pesos");
+            Console.ReadKey();
+        }
+        static void Main (String[] args)
+        {
+            Empresa emp = new Vectores.Empresa();
+            emp.CargaSueldos();
+            emp.ImprimeGastos();
+        }
+    }
+}
---
+++ Math/ConsoleApplication1/TutorialesYa/Vectores/Puntajes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApplication1.TutorialesYa.Vectores
+{
+    class Puntajes
+    {
+        private float[] puntajes;
+        private float promedio;
+
+        public void IngresarDatos()
+        {
+            puntajes = new float[8];
+            for (int x = 0; x < 8; x++)
+            {
+                Console.WriteLine("Ingrese el puntaje:");
+                puntajes[x] = float.Parse(Console.ReadLine());
+            }
+        }
+        public void CalcularPromedio()
+        {
+            float suma = 0;
+            for (int x = 0; x < 8; x++)
+            {
+                suma = suma + puntajes[x];
+            }
+            promedio = suma / 8;
+            Console.WriteLine("El promedio de los puntajes ingresados es de " + promedio);
+        }
+        public void MayorMenor()
+        {
+            int mayor = 0;
+            int menor = 0;
+            for (int x = 0; x < 8; x++)
+            {
+                if(puntajes[x] < promedio)
+                {
+                    menor++;
+                }
+                else
+                {
+                    if(puntajes[x] > promedio)
+                    {
+                        mayor++;
+                    }
+                }
+            }
+            Console.WriteLine("Se ingresaron " + menor + " puntajes menores al promedio.");
+            Console.WriteLine("Se ingresaron " + mayor + " puntajes mayores al promedio.");
+            Console.ReadKey();
+        }
+        static void Main (String[] args)
+        {
+            Puntajes pun = new Vectores.Puntajes();
+            pun.IngresarDatos();
+            pun.CalcularPromedio();
+            pun.MayorMenor();
+        }
+    }
+}

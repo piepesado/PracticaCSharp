718dc21a4f2957f239cb1846f6fc7cf0a76f9fbe
diegon <diegon@MIW-NB33>
2016-11-30 15:04:06 -0300

    Agregué clases de vectores.

---
+++ Math/ConsoleApplication1/TutorialesYa/Vectores/SumaVectores.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApplication1.TutorialesYa.Vectores
+{
+    class SumaVectores
+    {
+        private int[] vector1;
+        private int[] vector2;
+        private int[] vecSuma;
+
+        public void CargaVectores()
+        {
+            vector1 = new int[4];
+            vector2 = new int[4];
+            Console.WriteLine("Carga de 2 grupos de 4 números cada uno.");
+           
+            for ( int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Ingrese un valor 1er vector:");
+                vector1[i] = int.Parse(Console.ReadLine());
+            }            
+            for ( int f = 0; f < 4; f++)
+            {
+                Console.WriteLine("Ingrese un valor 2do vector:");
+                vector2[f] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void Suma()
+        {
+            vecSuma = new int[4];
+            for ( int x = 0; x < 4; x++)
+            {
+                vecSuma[x] = vector1[x] + vector2[x];
+            }
+            Console.WriteLine("El resultado de la suma de los dos vectores es:");
+            for (int z=0; z < 4; z++)
+            {
+                Console.WriteLine(vecSuma[z]);                
+            }
+            Console.ReadKey();
+        }
+        public static void Main(String[] args)
+        {
+            SumaVectores sumVec = new SumaVectores();
+            sumVec.CargaVectores();
+            sumVec.Suma();
+        }
+    }
+}
---
+++ Math/ConsoleApplication1/TutorialesYa/Vectores/VectordeOcho.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApplication1.TutorialesYa.Vectores
+{
+    class VectordeOcho
+    {
+        private int[] ochoValores;
+       
+        public void CargaValores()
+        {
+            ochoValores = new int[8];
+            Console.WriteLine("Ingrese 8 valores:");
+            for (int i = 0; i <8; i++)
+            {
+                Console.WriteLine("Digite valor:");
+                ochoValores[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void AcumularElementos()
+        {
+            int suma = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                suma = suma + ochoValores[i];
+            }
+            Console.WriteLine("La suma de los valores ingresados es: " + suma);
+        }
+
+        public void ValorMayores36()
+        {
+            int suma = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (ochoValores[i] > 36)
+                {
+                    suma = suma + ochoValores[i];
+                }
+            }
+            Console.WriteLine("La suma de los los valores ingresados mayores a 36 es: " + suma);            
+        }
+
+        public void CantidadMayore50()
+        {
+            int cantidad = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (ochoValores[i] > 50)
+                {
+                    cantidad++;
+                }
+            }
+            Console.WriteLine("Se ingresaron " + cantidad + " valores mayores a 50.");
+            Console.ReadKey();
+        }
+
+        public static void Main(String[] args)
+        {
+            VectordeOcho vector8 = new VectordeOcho();
+            vector8.CargaValores();
+            vector8.AcumularElementos();
+            vector8.ValorMayores36();
+            vector8.CantidadMayore50();
+        }
+    }
+}
